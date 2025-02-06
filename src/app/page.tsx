bae38e780fa403ac5b23d3ca9f76a9236c9a006f
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2025-02-06 01:26:02 -0300

    implement dashboard layout and navigation; update sign-in redirection to dashboard; enhance home page with sign-in and sign-up buttons; add mobile sidebar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,30 @@
 
-import { getCurrent } from "@/features/auth/actions";
-import { UserButton } from "@/features/auth/components/user-button";
+
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
+import { Button } from "@/components/ui/button";
+import { getCurrent } from "@/features/auth/actions";
+
 export default async function Home() {
+
+
+
   const user = await getCurrent();
 
-  if (!user) redirect("/sign-in");
+  if (user) redirect("/dashboard");
 
   return (
-    <div >
-      <UserButton />
-    </div>
+    <main className="flex flex-col items-center  h-screen">
+      <span className="text-2xl p-3">Pagina Principal</span>
+
+      <Button>
+        <Link href="/sign-in">Entrar</Link>
+      </Button>
+
+      <Button>
+        <Link href="/sign-up">Cadastrar</Link>
+      </Button>
+    </main>
   );
 }
