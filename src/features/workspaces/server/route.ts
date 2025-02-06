6a3e00ff0fd1911b8afc1fe5ab2b32f1160e859f
Welbert-Soares <welbertsoares6@gmail.com>
2025-02-06 17:34:55 -0300

    add workspace switcher component; implement API integration for fetching workspaces; update sidebar to include workspace switcher

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -6,47 +6,58 @@ import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACE_ID } from "@/config";
 import { createWorkspaceSchema } from "../schemas";
 import { sessionMiddleware } from "@/lib/sesssion-middleware";
 
-const app = new Hono().post(
-	"/",
-	zValidator("form", createWorkspaceSchema),
-	sessionMiddleware,
-	async (c) => {
+const app = new Hono()
+	.get("/", sessionMiddleware, async (c) => {
 		const databases = c.get("databases");
-		const storage = c.get("storage");
-		const user = c.get("user");
-
-		const { name, image } = c.req.valid("form");
-
-		let uploadedImageUrl: string | undefined;
-
-		if (image instanceof File) {
-			const file = await storage.createFile(
-				IMAGES_BUCKET_ID,
-				ID.unique(),
-				image,
-			);
-
-			const arrayBuffer = await storage.getFilePreview(
-				IMAGES_BUCKET_ID,
-				file.$id,
-			);
-
-			uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`;
-		}
-
-		const workspaces = await databases.createDocument(
-			DATABASE_ID,
-			WORKSPACE_ID,
-			ID.unique(),
-			{
-				name,
-				userId: user.$id,
-				imageUrl: uploadedImageUrl,
-			},
+		
+		const workspaces = await databases.listDocuments(
+			DATABASE_ID, 
+			WORKSPACE_ID
 		);
 
 		return c.json({ data: workspaces });
-	},
-);
+	})
+	.post(
+		"/",
+		zValidator("form", createWorkspaceSchema),
+		sessionMiddleware,
+		async (c) => {
+			const databases = c.get("databases");
+			const storage = c.get("storage");
+			const user = c.get("user");
+
+			const { name, image } = c.req.valid("form");
+
+			let uploadedImageUrl: string | undefined;
+
+			if (image instanceof File) {
+				const file = await storage.createFile(
+					IMAGES_BUCKET_ID,
+					ID.unique(),
+					image,
+				);
+
+				const arrayBuffer = await storage.getFilePreview(
+					IMAGES_BUCKET_ID,
+					file.$id,
+				);
+
+				uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`;
+			}
+
+			const workspaces = await databases.createDocument(
+				DATABASE_ID,
+				WORKSPACE_ID,
+				ID.unique(),
+				{
+					name,
+					userId: user.$id,
+					imageUrl: uploadedImageUrl,
+				},
+			);
+
+			return c.json({ data: workspaces });
+		},
+	);
 
 export default app;
